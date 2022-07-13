d03903b27afd9f8e4bda72c701f6de7ef3fdd3d3
pius mary <108791720+Piusmary@users.noreply.github.com>
2022-07-13 14:06:48 +0000

    my solution to  assignment 1&2

--- src/assignment.js
+++ src/assignment.js
@@ -13,10 +13,14 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
-function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+function sumOfNumbers(arrayOfNumbers){
+let sum = arrayOfNumbers.reduce((previousValue,currentValue) => 
+  {
+  return previousValue + currentValue;
+});
+  return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+ assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +32,9 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    return arrayOfNumbers.filter(number => number % 2 === 0).length
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+ assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
